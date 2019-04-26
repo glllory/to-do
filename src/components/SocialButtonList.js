fca1b77f15acf09838809b93613382044288823f
Majd Saleh <mjd.s.m@hotmail.com>
2019-04-26 22:25:13 +0300

    Social Media Buttons style

--- src/components/SocialButtonList.js
+++ src/components/SocialButtonList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 import { Jumbotron } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './bootstrap-social.css';
 
 
