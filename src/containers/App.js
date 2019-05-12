a3481e56acec2804a776250977bf6612d0d31f12
Majd Saleh <mjd.s.m@hotmail.com>
2019-05-12 17:32:39 +0300

    deploy

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Login from '../components/Login';
 import ToDoApp from '../components/ToDoApp';
 import withAuthentication from '../containers/withAuthentication';
@@ -7,12 +7,12 @@ import withAuthentication from '../containers/withAuthentication';
 class App extends Component {
   render() {
     return (
-      <Router basename='/'>
+      <HashRouter basename='/'>
         <Switch>
           <Route path="/" exact component={Login} />
           <Route path="/todoapp" component={withAuthentication(ToDoApp)} />
         </Switch>
-      </Router>
+      </HashRouter>
     );
   }
 }
