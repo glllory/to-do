3616e10134207fdc3995ee9c3c0b9e74e170a66d
Majd Saleh <mjd.s.m@hotmail.com>
2019-06-21 04:44:00 +0300

    Updates

---
+++ precache-manifest.85f61ac7237e911fe3efb46f5df9c934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18745a8d97750456430af7877fc0e830",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "0baa66f05677abe5daaa",
+    "url": "/to-do/static/css/2.947d3333.chunk.css"
+  },
+  {
+    "revision": "40cc4976feb4890174a1",
+    "url": "/to-do/static/css/main.0eabd2f6.chunk.css"
+  },
+  {
+    "revision": "0baa66f05677abe5daaa",
+    "url": "/to-do/static/js/2.2523d019.chunk.js"
+  },
+  {
+    "revision": "40cc4976feb4890174a1",
+    "url": "/to-do/static/js/main.009c6a8a.chunk.js"
+  },
+  {
+    "revision": "7c4529cb5998ccf5e9e5",
+    "url": "/to-do/static/js/runtime~main.c7e19ee4.js"
+  }
+]);
